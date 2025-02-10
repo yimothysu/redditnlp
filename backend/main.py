7796199b1e93a1e4816a7531b2fc354337187ad2
Grace Hu <88510107+gracelhu@users.noreply.github.com>
2025-02-10 01:25:46 -0500

    optimized api response time
    - changed comment tree depth to 0
    - changed post limit to 200

--- backend/main.py
+++ backend/main.py
@@ -2,27 +2,24 @@ from http.client import BAD_REQUEST
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import datetime
+import time 
 import matplotlib.pyplot as plt
 
 from reddit import get_comments, reddit
 from plot_post_distribution import plot_post_distribution
 
 app = FastAPI(title="RedditNLP API")
-post_limit = 10000
+post_limit = 200
 
 class RedditPost(BaseModel):
     title: str
     score: int
     url: str
     created_utc: float
-    # num_comments: int
-    # comments: list[str]
+    num_comments: int
+    comments: list[str]
 
-
-# if time_filter = year, default view is to slice by month 
-# if time_filter = month, default view is to slice by week 
-# if time_filter = week, default view is to slice by day 
-@app.get("/sample/{subreddit}", response_model=list)
+@app.get("/sample/{subreddit}", response_model=dict)
 async def sample_subreddit(
     subreddit: str, time_filter: str = "year", limit: int = post_limit, sort_by: str = "top"
 ):
@@ -51,20 +48,47 @@ async def sample_subreddit(
         posts = subreddit_instance.controversial(limit=limit, time_filter=time_filter)
 
     posts_list = [
-        RedditPost(
-            title=post.title,
-            score=post.score,
-            url=post.url,
-            created_utc=post.created_utc
-            #num_comments=post.num_comments,
-            #comments=get_comments(post),
-        )
-        for post in posts
-    ]
+            RedditPost(
+                title=post.title,
+                score=post.score,
+                url=post.url,
+                created_utc=post.created_utc,
+                num_comments=post.num_comments,
+                comments= get_comments(post)
+            )
+            for post in posts
+        ]
+    sorted_slice_to_posts = slice_posts_list(posts_list, time_filter)
+    #plot_post_distribution(subreddit, time_filter, sorted_slice_to_posts)
+    return sorted_slice_to_posts
 
-    plot_post_distribution(subreddit, time_filter, posts_list)
-    return posts_list
+# posts_list is a list of RedditPost objects
+# function returns a map where:
+#   key = date of slice (Ex: 07/24)
+#   value = vector of RedditPost objects for slice 
+#
+# if time_filter = year, slice by month 
+# if time_filter = month, slice by week 
+# if time_filter = week, slice by day 
 
+def slice_posts_list(posts_list, time_filter):
+    slice_to_posts = dict()
+    for post in posts_list:
+        # convert post date from utc to readable format 
+        date_format = ''
+        if time_filter == 'year': date_format = '%m-%y' # slicing a year by months 
+        else: date_format = '%d-%m' # slicing a month and week by days 
+        post_date = datetime.datetime.fromtimestamp(post.created_utc).strftime(date_format)
+
+        if post_date not in slice_to_posts:
+            slice_to_posts[post_date] = []
+        slice_to_posts[post_date].append(post)
+
+    # sort slice_to_posts by date
+    dates = list(slice_to_posts.keys())
+    sorted_months = sorted(dates, key=lambda x: datetime.datetime.strptime(x, date_format))
+    sorted_slice_to_posts = {i: slice_to_posts[i] for i in sorted_months}
+    return sorted_slice_to_posts
 
 if __name__ == "__main__":
     import uvicorn
--- backend/reddit.py
+++ backend/reddit.py
@@ -2,8 +2,11 @@ import os
 
 from dotenv import load_dotenv
 import praw
+from praw.models import MoreComments
 
 load_dotenv()
+
+praw.__dict__["_warn_async"] = False
 
 reddit = praw.Reddit(
     client_id=os.environ["REDDIT_CLIENT_ID"],
@@ -11,7 +14,17 @@ reddit = praw.Reddit(
     user_agent="reddit sampler",
 )
 
+# too slow 
+# for getting the comments of ~1000 posts, i was waiting for minutes, and then eventually 
+# it just crashed with a TooManyRequests error 
+# I just realized using try and except is TOO slow 
 
 def get_comments(post):
-    comment_list = post.comments.list()
-    return [comment.body for comment in comment_list]
+    # Getting just the first 20 comments to speed up program 
+    post_comments = []
+    post.comments.replace_more(limit=0)
+    for comment in post.comments[slice(20)]:
+        if isinstance(comment, MoreComments): continue
+        post_comments.append(comment.body)
+    print('len(post_comments): ', len(post_comments))
+    return post_comments 
