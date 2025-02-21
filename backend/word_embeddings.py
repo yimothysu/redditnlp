1ab78ea1989e10432abe7885f3a660ea55633151
coderliv <92771223+coderliv@users.noreply.github.com>
2025-02-21 00:30:24 -0500

    implement word embedding functions

---
+++ backend/word_embeddings.py
@@ -0,0 +1,34 @@
+import gensim.downloader as api
+#from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+
+model = api.load("word2vec-google-news-300")
+
+def get_word_embeddings(word_list):
+    word_embeddings = []
+    words = []
+    for word in word_list:
+        if word in model.key_to_index:
+            word_embeddings.append(model[word])
+            words.append(word)
+    return words, word_embeddings
+
+def get_2d_embeddings(word_list):
+    words, embeddings = get_word_embeddings(word_list)
+    if len(words) <= 1:
+        return None
+    if embeddings:
+        pca = PCA(n_components=2)
+        #tsne = TSNE(n_components = 2, perplexity = min(25, len(words) - 1), max_iter=500)
+        np_embedding = np.array(embeddings)
+        coordinates_2d = pca.fit_transform(np_embedding)#tsne.fit_transform(np.array(embeddings))
+
+        word_to_coordinates = {}
+        for word, embedding in zip(words, coordinates_2d):
+            word_to_coordinates[word] = (embedding[0], embedding[1])
+        return word_to_coordinates
+    return None
+
+print(get_2d_embeddings(["hello", "how", "are", "you"]))
