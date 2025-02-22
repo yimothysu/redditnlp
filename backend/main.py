ac9d2c1e10d4945e5c2b36a0734dfa72857ff420
Grace Hu <88510107+gracelhu@users.noreply.github.com>
2025-02-21 20:45:04 -0500

    implemented error handling + sped up n-gram & NER analysis

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 
 from praw.models import MoreComments
 import asyncio
+import time 
 
 #from reddit import get_comments, reddit
 from plot_post_distribution import plot_post_distribution
@@ -36,21 +37,26 @@ class SubredditNLPAnalysis(BaseModel):
 
 async def fetch_post_data(post):
     # get the post's comments 
-    comments = await post.comments()
-    post_comments = []  
-    for comment in comments:
-        if isinstance(comment, MoreComments): continue
-        if hasattr(comment, "body"):
-            post_comments.append(comment.body)
+    #print("inside fetch_post_data")
+    try:
+        comments = await post.comments()
+        post_comments = []  
+        for comment in comments:
+            if isinstance(comment, MoreComments): continue
+            if hasattr(comment, "body"):
+                post_comments.append(comment.body)
     
-    return RedditPost(
-        title=post.title,
-        score=post.score,
-        url=post.url,
-        created_utc=post.created_utc,
-        num_comments=post.num_comments,
-        comments=post_comments
-    )
+        return RedditPost(
+            title=post.title,
+            score=post.score,
+            url=post.url,
+            created_utc=post.created_utc,
+            num_comments=post.num_comments,
+            comments=post_comments
+        )
+    except Exception as e:
+        print("could not get post's comments because: ", e)
+    
 
 @app.get("/sample/{subreddit}", response_model=SubredditNLPAnalysis)
 async def sample_subreddit(
@@ -61,6 +67,12 @@ async def sample_subreddit(
     client_secret=os.environ["REDDIT_CLIENT_SECRET"],
     user_agent="reddit sampler",
     )   
+
+    # !!! WARNING !!!
+    # sometimes PRAW will be down, or the # of posts you're trying to get at once 
+    # is too much. PRAW will often return a 500 HTTP response in this case.
+    # Try to: reduce post_limit, or wait a couple minutes 
+
     post_limit = time_filter_to_post_limit[time_filter]
     SORT_METHODS = ["top", "controversial"]
     TIME_FILTERS = ["hour", "day", "week", "month", "year", "all"]
@@ -77,13 +89,32 @@ async def sample_subreddit(
             detail=f"Invalid sort method. Must be one of {SORT_METHODS}.",
         )
 
-    subreddit_instance = await reddit.subreddit(subreddit)
-    posts = [post async for post in subreddit_instance.top(limit=post_limit, time_filter=time_filter)]
-    # Fetch post data concurrently
-    posts_list = await asyncio.gather(*(fetch_post_data(post) for post in posts))
+    try:
+        t1 = time.time()
+        subreddit_instance = await reddit.subreddit(subreddit)
+        print('able to create subreddit_instance')
+        posts = [post async for post in subreddit_instance.top(limit=post_limit, time_filter=time_filter)]
+        t2 = time.time()
+        print("fetched all reddit posts in: ", t2-t1)
+    except Exception as e:
+        print('could not get top subreddit posts because: ', e)
     
+    try:
+        # Fetch post data concurrently
+        posts_list = await asyncio.gather(*(fetch_post_data(post) for post in posts))
+        #await reddit.close()
+        t3 = time.time()
+        print("fetched ", len(posts_list), " posts' comments in: ", t3-t2)
+    except Exception as e:
+        print('could not get post comments because: ', e)
+
+    # remove all posts which are type None 
+    posts_list = [post for post in posts_list if post is not None]
+    print("# of non None posts: ", len(posts_list))
+
     sorted_slice_to_posts = slice_posts_list(posts_list, time_filter)
-    print('finished getting sorted_slice_to_posts')
+    t4 = time.time()
+    print('finished getting sorted_slice_to_posts in: ', t4-t3)
     #plot_post_distribution(subreddit, time_filter, sorted_slice_to_posts)
     top_n_grams, top_named_entities = get_subreddit_analysis(sorted_slice_to_posts)
     analysis = SubredditNLPAnalysis(
--- backend/subreddit_nlp_analysis.py
+++ backend/subreddit_nlp_analysis.py
@@ -3,6 +3,9 @@ import re
 from collections import Counter
 from pydantic import BaseModel
 from typing import Dict, List, Tuple 
+import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+import time
 
 # NLP related imports 
 from sklearn.feature_extraction.text import CountVectorizer
@@ -10,8 +13,12 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import spacy 
+
+start = time.time()
 nltk.download('stopwords')
 nltk.download('punkt')
+end = time.time()
+print('finished downloading nltk stopwords and punkt in: ', end-start)
 
 # Perhaps naive filtering of named entities
 # Returns true iff name is a valid named entity name
@@ -30,24 +37,30 @@ def filter_named_entity(name: str) -> bool:
             return False
     return True
 
-# This function is called in the FastAPI and is what gets returned from FastAPI 
-def get_subreddit_analysis(sorted_slice_to_posts):
-    # Perform n-gram analysis 
+def get_subreddit_analysis(sorted_slice_to_posts): # called in main.py's FastAPI
+    texts = []
+    dates = []
+    for date, posts in sorted_slice_to_posts.items():
+        post_content = ' '.join([post.title + ' '.join(post.comments) for post in posts])
+        texts.append(post_content)
+        dates.append(date)
+    # Perform n-gram analysis - just only take ~ 1-2 seconds 
+    # ProcessPoolExecutor and ThreadPoolExecuter do NOT help speed up n-gram analysis 
+    t1 = time.time()
     top_n_grams = dict() 
-    for date, posts in sorted_slice_to_posts.items():
-        post_content = ' '.join([post.title + ' '.join(post.comments) for post in posts])
-        filtered_post_content = preprocess_text_for_n_grams(post_content)
-        top_n_grams[date] = get_top_ngrams_sklearn([filtered_post_content])
-    print('finished n-gram analysis')
+    for i in range(0, len(dates)):
+        filtered_post_content = preprocess_text_for_n_grams(texts[i]) # optimized and fast now 
+        top_n_grams[dates[i]] = get_top_ngrams_sklearn([filtered_post_content]) # very fast 
+    t2 = time.time()
+    print('finished n-gram analysis in: ', t2-t1)
 
-    # Perform NER analysis 
-    nlp = spacy.load("en_core_web_sm")
+    # Perform NER analysis - takes ~ 20 seconds 
+    nlp = spacy.load("en_core_web_sm", disable=["parser", "tagger"])
     top_named_entities = dict() 
-    for date, posts in sorted_slice_to_posts.items():
-        post_content = ' '.join([post.title + ' '.join(post.comments) for post in posts])
-        doc = nlp(post_content)
+    for doc, date in zip(nlp.pipe(texts, batch_size=50), dates): # Processing all texts in batches
         top_named_entities[date] = postprocess_named_entities(doc)
-    print('finished NER analysis')
+    t3 = time.time()
+    print('finished NER analysis in: ', t3-t2)
     
     return top_n_grams, top_named_entities
 
@@ -66,15 +79,16 @@ def postprocess_named_entities(doc):
     return filtered_top_named_entities 
     
 def preprocess_text_for_n_grams(post_content):
+    partial_stop_words = {'wiki', 'co', 'www.', 'reddit', '.com', 'autotldr', 'http'}
+    full_stop_words = {'or', 'and', 'you', 'that', 'the', 'co', 'en', 'np', 'removed', 'next'}
+    full_stop_words = set(stopwords.words('english')).union(full_stop_words)
+    
     words = word_tokenize(post_content) # tokenize post_content 
-    filtered_words = [re.sub(r'[^\w\s]', '', word) for word in words]  # Remove non-word characters
+    # Remove non-word characters, remove whitespace before and after word, make all lowercase
+    filtered_words = [re.sub(r'[^\w\s]', '', word).lower().strip() for word in words] 
     filtered_words = [word for word in filtered_words if word]  # Remove empty strings
-    filtered_words = [word.lower().strip() for word in words] # remove whitespace before and after word + make all lowercase 
-    partial_stop_words = ['wiki', 'co', 'www.', 'reddit', '.com', 'autotldr', 'http']
-    full_stop_words = ['or', 'and', 'you', 'that', 'the', 'co', 'en', 'np', 'removed', 'next']
-    filtered_words = [word for word in words if word not in stopwords.words('english')]
-    filtered_words = [word for word in words if not any(exclude in word.lower() for exclude in partial_stop_words)]
-    filtered_words = [word for word in filtered_words if word not in full_stop_words]
+    filtered_words = [word for word in filtered_words if word not in full_stop_words and 
+                      not any(exclude in word for exclude in partial_stop_words)]
     filtered_post_content = ' '.join(filtered_words)
     return filtered_post_content 
 
