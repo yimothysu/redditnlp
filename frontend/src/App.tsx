8bc72034bb6910fff3faeb826f2dfa5cbd15f6e9
coderliv <92771223+coderliv@users.noreply.github.com>
2025-03-30 15:29:05 -0400

    fix build errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 
 import Home from "../pages/Home.tsx";
 import RedditPage from "../pages/RedditPage.tsx";
-import CompareRedditPage from "../pages/CompareRedditPage.tsx";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -12,10 +11,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/subreddit/:name" element={<RedditPage />}></Route>
-      <Route
-        path="/compare-reddit-page"
-        element={<CompareRedditPage />}
-      ></Route>
     </Routes>
   );
 }
