3649c8cb02e598ea5da18635ef8c6c3b82b3a209
Grace Hu <88510107+gracelhu@users.noreply.github.com>
2025-04-19 22:30:44 -0400

    add more subreddits to database_populator.py

--- backend/src/database/database_populator.py
+++ backend/src/database/database_populator.py
@@ -24,8 +24,6 @@ subreddits = [
     "technology",
     "100YearsAgo",
     "AskHistorians",
-    "conspiracytheories",
-    "LetsNotMeet",
     "unpopularopinion",
     "philosophy",
     "mentalhealth",
@@ -35,7 +33,34 @@ subreddits = [
     "personalfinance",
     "changemyview",
     "LateStageCapitalism",
-    "UpliftingNews"
+    "UpliftingNews",
+    "ApplyingToCollege",
+    "conspiracytheories",
+    "linguistics",
+    "lgbt",
+    "DrugNerds",
+    "Bitcoin",
+    "Futurology",
+    "findapath",
+    "Parenting",
+    "4chan",
+    "TheRedPill",
+    "TheBluePill",
+    "SkincareAddiction",
+    "Conservative",
+    "Libertarian",
+    "antiwork",
+    "Christianity",
+    "uofm",
+    "Harvard",
+    "stanford", 
+    "mit",
+    "berkeley",
+    "UPenn",
+    "yale",
+    "columbia",
+    "princeton",
+    "ufl",
 ]
 
 async def fetch_subreddit_data(subreddit, time_filter):
@@ -55,15 +80,24 @@ async def fetch_subreddit_data(subreddit, time_filter):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Error: must pass in time filter argument")
+    if len(sys.argv) != 3:
+        print("Error: must pass in time filter argument and start from index")
         sys.exit(1)
     
     time_filter = sys.argv[1]
-    if(time_filter not in ['week', 'year', 'all']):
+    start_from_idx = sys.argv[2]
+    if time_filter not in ['week', 'year', 'all']:
         print("Error: time filter argument must be 'week', 'year', or 'all'")
         sys.exit(1)
     
-    for subreddit in subreddits:
+    if not isinstance(start_from_idx, int):
+        print("Error: start from index argument must be an integer")
+        sys.exit(1)
+    
+    if start_from_idx < 0 or start_from_idx >= len(subreddits):
+        print("Error: not a valid start from index argument. Must be between 0 and ", len(subreddits), " inclusive")
+    
+    for start_from_idx in range(len(subreddits)):
+        subreddit = subreddits[start_from_idx]
         print(f"Working on subreddit {subreddit}")
         asyncio.run(fetch_subreddit_data(subreddit, time_filter))
