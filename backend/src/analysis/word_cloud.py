61709b57c5d49b5dc749683925e1d8c177ffcf70
Angela Wang <angelawang.aw@outlook.com>
2025-04-22 13:42:55 -0400

    Separate named_entities_to_dictionary into own function

--- backend/src/analysis/word_cloud.py
+++ backend/src/analysis/word_cloud.py
@@ -6,15 +6,7 @@ import base64
 # Takes in dictionary of named entities
 def generate_word_cloud(named_entities):    
     # Create dictionary of named entities and their frequencies
-    fullDict = {}
-    
-    # Add named entities to dictionary
-    for entity_list in named_entities.values():        
-        for entity in entity_list:
-            if entity[0] not in fullDict:
-                fullDict[entity[0]] = entity[1]
-            elif entity[0] in fullDict:
-                fullDict[entity[0]] += entity[1]
+    fullDict = named_entities_to_dictionary(named_entities)
     
     wc = WordCloud(background_color="white", max_words=2000, contour_width=3, contour_color='steelblue')
     
@@ -27,26 +19,17 @@ def generate_word_cloud(named_entities):
     
     return img_base64
 
-# Takes in dictionary of named entities
-def generate_by_date(named_entities):
+def named_entities_to_dictionary(named_entities):
+    # Create dictionary of named entities and their frequencies
+    fullDict = {}
+    
     # Add named entities to dictionary
-    for date, entity_list in named_entities.items():
-        
-        tempDict = {}        
+    for entity_list in named_entities.values():        
         for entity in entity_list:
-            if entity[0] not in tempDict:
-                tempDict[entity[0]] = entity[1]
-            elif entity[0] in tempDict:
-                tempDict[entity[0]] += entity[1]
-        
-        # If no named entities for date, skip
-        if not entity_list:
-            continue
-        
-        wc = WordCloud(background_color="white", max_words=2000, contour_width=3, contour_color='steelblue')
-
-                
-        wc.generate_from_frequencies(tempDict)
-        print("Word cloud generated")
-        
-        return wc.to_array()
+            if entity[0] not in fullDict:
+                fullDict[entity[0]] = entity[1]
+            elif entity[0] in fullDict:
+                fullDict[entity[0]] += entity[1]
+    
+    return fullDict
+    
