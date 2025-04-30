4870910e96f3ca32d8bb15c53a776c7d07b480e8
Grace Hu <88510107+gracelhu@users.noreply.github.com>
2025-04-29 20:24:07 -0400

    move EntityPicture into its own file

---
+++ frontend/src/components/analysis/EntityPicture.tsx
@@ -0,0 +1,31 @@
+import { entity_name_to_png } from "../../constants/named_entity_png.ts";
+ 
+export const EntityPicture = ({entity_name}: { entity_name: string }) => {
+    let picture_file_name = null;
+    if(entity_name in entity_name_to_png) {
+      picture_file_name = entity_name_to_png[entity_name];
+    }
+    else {
+      entity_name = entity_name.toLowerCase();
+      if(entity_name in entity_name_to_png) {
+        picture_file_name = entity_name_to_png[entity_name];
+      }
+      else if (entity_name.startsWith("the ") && entity_name.slice(4) in entity_name_to_png) {
+        picture_file_name = entity_name_to_png[entity_name.slice(4)];
+      }
+      else if (!entity_name.endsWith("s") && (entity_name + "s") in entity_name_to_png) {
+        picture_file_name = entity_name_to_png[entity_name + "s"];
+      }
+    }
+
+    if (picture_file_name != null) {
+      return (
+        <div className="shadow-xs rounded-sm p-1">
+          <img
+            src={"/named_entity_pics/" + picture_file_name}
+            className="w-9 object-cover cursor-pointer transition active:scale-95 active:brightness-90"
+          />
+        </div>
+      );
+    }
+  };
--- frontend/src/components/analysis/NamedEntities.tsx
+++ frontend/src/components/analysis/NamedEntities.tsx
@@ -4,7 +4,7 @@ import ButtonPopover from "../ButtonPopover";
 import { ColorKey } from "./ColorKey";
 import { formatDate } from "../../utils/dateUtils";
 import { ChevronRight } from "lucide-react";
-import { entity_name_to_png } from "../../constants/named_entity_png.ts";
+import { EntityPicture } from "./EntityPicture";
 
 interface NamedEntitiesProps {
   analysis: SubredditAnalysis;
@@ -72,36 +72,6 @@ const TopNamedEntitiesForDate: React.FC<TopNamedEntitiesForDateProps> = ({
   entities,
   timeFilter,
 }) => {
-
-  const EntityPicture = ({entity_name}: { entity_name: string }) => {
-    let picture_file_name = null;
-    if(entity_name in entity_name_to_png) {
-      picture_file_name = entity_name_to_png[entity_name];
-    }
-    else {
-      entity_name = entity_name.toLowerCase();
-      if(entity_name in entity_name_to_png) {
-        picture_file_name = entity_name_to_png[entity_name];
-      }
-      else if (entity_name.startsWith("the ") && entity_name.slice(4) in entity_name_to_png) {
-        picture_file_name = entity_name_to_png[entity_name.slice(4)];
-      }
-      else if (!entity_name.endsWith("s") && (entity_name + "s") in entity_name_to_png) {
-        picture_file_name = entity_name_to_png[entity_name + "s"];
-      }
-    }
-
-    if (picture_file_name != null) {
-      return (
-        <div className="shadow-xs rounded-sm p-1">
-          <img
-            src={"/named_entity_pics/" + picture_file_name}
-            className="w-9 object-cover cursor-pointer transition active:scale-95 active:brightness-90"
-          />
-        </div>
-      );
-    }
-  };
 
   return (
     <div
