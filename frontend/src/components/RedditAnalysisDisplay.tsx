6d9fa9d905647aab74968d346c61daa5cf0be6f5
Grace Hu <88510107+gracelhu@users.noreply.github.com>
2025-05-12 14:31:42 -0400

    remove 'sort by' on analysis page

--- frontend/src/components/RedditAnalysisDisplay.tsx
+++ frontend/src/components/RedditAnalysisDisplay.tsx
@@ -181,18 +181,6 @@ export default function RedditAnalysisDisplay({
               <option value="week">Week</option>
             </select>
           </div>
-          <div>
-            <label className="block text-sm text-center font-medium text-gray-700">
-              Sort By
-            </label>
-            <select
-              value={sortBy}
-              onChange={(e) => setSortBy(e.target.value)}
-              className="mt-1 block w-full px-1 py-2 text-base border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-            >
-              <option value="top">Top Posts</option>
-            </select>
-          </div>
           <div className="relative flex">
             {isBusy && !analysis && (
               <div className="">
